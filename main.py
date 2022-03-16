551f2eeef9b25129abf957be2074d6f0433cd278
Fedor Nevstruev <frodoofficial@mail.ru>
2022-03-16 13:18:51 +0300

    работает, но не выполняет задание полностью

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 with open("input.txt") as textFile:
     lines = [line.split() for line in textFile]
     textFile.close()
-print(lines.__str__())
+print("Input text: " + lines.__str__())
 
 
 def check(t_list, i):
@@ -23,21 +23,44 @@ def search(text_list):
 def copy_suitable_str(text_list, index_list):
     copied_str = []
     for i in range(len(index_list)):
-        s = text_list[index_list[i]].copy
+        s = text_list[index_list[i]][:]
         copied_str.append(s)
     return copied_str
 
 
-# t_list = ['a', 'a', '=', 'a', '+', '1']
-# for i in range(1, len(t_list)-3):
-#     print(check(t_list, i))
+def change(text_list, index_list, changed_str):
+    for i in range(len(index_list)):
+        text_list[index_list[i]] = changed_str[i]
+
+
+def change_str(copied_str):
+    changed_str = []
+    for i in range(len(copied_str)):
+        for j in range(len(copied_str[i])):
+            if (copied_str[i][j] == '=' and copied_str[i][j-1] == copied_str[i][j+1] and copied_str[i][j+2] == '+'
+                    and copied_str[i][j+3] == '1'):
+                c_list_b = copied_str[i][:j-1]
+                c_list_c = c_list_b + [copied_str[i][j-1], '+', '+']
+                changed_str.append(c_list_c)
+    return changed_str
+
+
 
 i_list = search(lines)
-print(i_list.__str__())
+print("Index of suitable str: " + i_list.__str__())
 s = copy_suitable_str(lines, i_list)
-print(s.__str__())
+print("Copy of suitable str: " + s.__str__())
+c = change_str(s)
+print("Changed str: " + c.__str__())
+change(lines, i_list, c)
+print("Changed text: " + lines.__str__())
+
+with open('output.txt', 'w') as testfile:
+    for row in lines:
+        testfile.write(' '.join([str(a) for a in row]) + '\n')
+testfile.close()
 
 
-# def change(text_list, index_list):
+
 
 
